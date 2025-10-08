c66a63757414a22ab625245d29339db58a2f1a64
gut23H <donapauladoceria@gmail.com>
2025-10-07 23:01:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,13 +5,19 @@ const multer = require('multer');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// Ativa CORS e JSON
 app.use(cors());
 app.use(express.json());
 
+// Serve arquivos estáticos da pasta public
+app.use(express.static('public'));
+
+// Rota de teste (opcional)
 app.get('/', (req, res) => {
-  res.send('Servidor Node.js rodando!');
+  res.sendFile(__dirname + '/public/index.html');
 });
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
+
